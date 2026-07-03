9c8f2c97ed82b9ed3efb7597e6f28edb026603d6
follyfoxe <sweetfollyfox@gmail.com>
2026-07-03 19:54:45 -0400

    Organize this thingy

--- Content/Items/Armor/Vanity/BlackRider/BlackRiderSet.cs
+++ Content/Items/Armor/Vanity/BlackRider/BlackRiderSet.cs
@@ -3,7 +3,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace WgMod.Content.Items.Armor.Vanity;
+namespace WgMod.Content.Items.Armor.Vanity.BlackRider;
 
 public static class BlackRiderSet
 {
