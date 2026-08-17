c77fa28923d592db76e255b305a08fe5ddcccb2a
MaimaiChubsUwU <maimaiowo12705@gmail.com>
2026-08-16 17:27:47 -0700

    GorgeistBeaten achievement

---
+++ Content/Achievements/GorgeistBeaten.cs
@@ -0,0 +1,16 @@
+﻿using WgMod.Content.NPCs.UndergroundDesert.GorgeistBoss;
+using Terraria.ModLoader;
+
+namespace WgMod.Content.Achievements;
+
+public class GorgeistBeaten : ModAchievement
+{
+	public override void SetStaticDefaults()
+	{
+		AddNPCKilledCondition(ModContent.NPCType<Gorgeist>());
+	}
+
+	public override Position GetDefaultPosition() => new Before("EYE_ON_YOU");
+
+	public override Position GetAdvisorPosition() => new Before("EYE_ON_YOU");
+}
