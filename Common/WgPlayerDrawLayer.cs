b6f5886a4695edd16a4b91e27e4580ee676aadeb
follycake <sweetfollyfox@gmail.com>
2026-08-26 20:28:36 -0400

    SpriteSet.GetOffset

--- Common/WgPlayerDrawLayer.cs
+++ Common/WgPlayerDrawLayer.cs
@@ -73,8 +73,7 @@ public class WgPlayerDrawLayer : PlayerDrawLayer
 
         int direction = ((drawInfo.playerEffect & SpriteEffects.FlipHorizontally) == 0).ToDirectionInt();
         Vector2 position = new Vector2((int)(drawInfo.Position.X - Main.screenPosition.X - drawInfo.drawPlayer.bodyFrame.Width / 2 + drawInfo.drawPlayer.width / 2), (int)(drawInfo.Position.Y - Main.screenPosition.Y + drawInfo.drawPlayer.height - drawInfo.drawPlayer.bodyFrame.Height)) + drawInfo.drawPlayer.bodyPosition + new Vector2(drawInfo.drawPlayer.bodyFrame.Width / 2, drawInfo.drawPlayer.bodyFrame.Height / 2);
-        position.X += stageData.OffsetX * direction;
-        position += new Vector2(set.DrawOffsetX * direction, set.DrawOffsetY * player.gravDir);
+        position += SpriteSet.GetOffset(set, stageData, direction, player.gravDir);
 
         if (Main.gameMenu)
             wg.UpdateAnimation();
@@ -150,8 +149,8 @@ public class WgPlayerDrawLayer : PlayerDrawLayer
 
         int direction = drawPlayer.direction;
         Vector2 layerPos = drawPos;
-        layerPos.X += stageData.OffsetX * direction;
-        layerPos += new Vector2(set.DrawOffsetX * direction, (set.DrawOffsetY - 8f) * drawPlayer.gravDir);
+        layerPos += SpriteSet.GetOffset(set, stageData, direction, drawPlayer.gravDir);
+        layerPos.Y -= 8f;
 
         void DrawLayers(SpriteSet.Layer[] layers)
         {
